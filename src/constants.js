5ea200383e099aeb0d00d44d474a8f0b7e879d0e
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-03-28 20:42:09 -0400

    Remove unused REGEX constant

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,5 @@
 export const STRING = "string";
 export const UNDEFINED = "undefined";
 
-export const REGEX = {
-	false: /^(F|f)alse$/,
-	null: /^(N|n)ull$/,
-	true: /^(T|t)rue$/,
-};
-
 export const MAX_DEPTH = 100;
 export const MAX_STRING_SIZE = 100000;
